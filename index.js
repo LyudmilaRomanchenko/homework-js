7f1f0ec2267ac1d6110bd7b949584f5a643f068e
LyudmilaRomanchenko <nowbeauty31@gmail.com>
2021-06-02 16:31:54 +0300

    Добавляет 9 задач

--- index.js
+++ index.js
@@ -256,7 +256,140 @@ const customer = {
   
 
 /*========================= Задача-7 =============================*/
+/* Метод call
+Бывают ситуации когда функцию нужно вызвать в контексте какого-то объекта, при этом функция не является его методом. Для этого у функций есть методы call и apply.
+
+foo.call(obj, arg1, arg2, ...)
+Метод call вызовет функцию foo так, что в this будет ссылка на объект obj, а также передаст аргументы arg1, arg2 и т. д.
+
+function greetGuest(greeting) {
+  console.log(`${greeting}, ${this.username}.`);
+}
+
+const mango = { username: "Манго" };
+const poly = { username: "Поли" };
+
+greetGuest.call(mango, "Добро пожаловать");
+// Добро пожаловать, Манго.
+
+greetGuest.call(poly, "С приездом");
+// С приездом, Поли.
+Задание
+Сервису приготовления и доставки еды требуется функция генерации сообщений о статусе заказа.
+
+Дополни функцию composeMessage(position) так, чтобы она возвращала строку в формате 'Готовим <блюдо> для <почта>. Ваш заказ <позиция>-й в очереди.' Позиция это значение параметра position - позиция элемента в массиве (на единицу больше чем индекс).
+
+Не объявляй дополнительные параметры функции composeMessage(position).
+Используй call для вызова функции в контексте одного объекта-заказа.
+Используй this в теле функции для доступа к свойствам объекта-заказа в контексте которого она была вызывана.
+Дополни код так, чтобы в переменной messages получился массив сообщений о статусе заказов из массива orders с помощью цикла for.
+
+Тесты
+Объявлена переменная orders.
+Значение переменной orders это исходный массив объектов-заказов.
+Объявлена функция composeMessage(position).
+У функции composeMessage объявлен один параметр position.
+В теле функции composeMessage используется this.
+Функция composeMessage вызывается методом call внутри цикла for.
+Объявлена переменная messages.
+Значение переменной messages это массив ['Готовим Burger для solomon@topmail.ua. Ваш заказ 1-й в очереди.', 'Готовим Pizza для artemis@coldmail.net. Ваш заказ 2-й в очереди.', 'Готовим Taco для jacob@mail.com. Ваш заказ 3-й в очереди.']. */
+
+const orders = [
+  { email: 'solomon@topmail.ua', dish: 'Burger' },
+  { email: 'artemis@coldmail.net', dish: 'Pizza' },
+  { email: 'jacob@mail.com', dish: 'Taco' },
+];
+
+// Пиши код ниже этой строки
+function composeMessage(position) {
+  return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
+	
+}
+
+const messages = [];
+for (let i = 0; i < orders.length; i += 1) {
+    	messages.push(composeMessage.call(orders[i], i+1));
+    }
+
+/*========================= Задача-8 =============================*/
+/* Метод apply
+Метод apply это аналог метода call за исключением того, что синтаксис передачи аргументов требует не перечисление, а массив, даже если аргумент всего один.
+
+foo.call(obj, arg1, arg2, ...)
+
+foo.apply(obj, [arg1, arg2, ...])
+Метод apply вызовет функцию foo так, что в this будет ссылка на объект obj, а также передаст элементы массива как отдельные аргументы arg1, arg2 и т. д. На практике, в основном используется метод call.
+
+function greetGuest(greeting) {
+  console.log(`${greeting}, ${this.username}.`);
+}
+
+const mango = { username: "Манго" };
+const poly = { username: "Поли" };
+
+greetGuest.apply(mango, ["Добро пожаловать"]); // Добро пожаловать, Манго.
+greetGuest.apply(poly, ["С приездом"]); // С приездом, Поли.
+Задание
+Выполни рефакторинг кода так, чтобы функция composeMessage(position) вызывалась методом apply. */
+
+const orders = [
+  { email: "solomon@topmail.ua", dish: "Burger" },
+  { email: "artemis@coldmail.net", dish: "Pizza" },
+  { email: "jacob@mail.com", dish: "Taco" },
+];
+
+// Пиши код ниже этой строки
+function composeMessage(position) {
+  return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
+}
+
+const messages = [];
+for (let i = 0; i < orders.length; i++) {
+  const msg = composeMessage.apply(orders[i], [i + 1]);
+  messages.push(msg);
+}
+
+
+/*========================= Задача-9 =============================*/
+/* Метод bind
+Методы call и apply вызывают функцию «на месте», то есть сразу. Но в случае колбэк-функций, когда необходимо не сразу вызвать функцию, а передать ссылку на неё, причём с привязанным контекстом, используется метод bind.
+
+foo.bind(obj, arg1, arg2, ...)
+Метод bind создаёт и возвращает копию функции foo с привязанным контекстом obj и аргументами arg1, arg2 и т. д. Получается копия функции которую можно передать куда угодно и вызвать когда угодно.
+
+function greet(clientName) {
+  return `${clientName}, добро пожаловать в «${this.service}».`;
+}
+
+const steam = { service: 'Steam' };
+const steamGreeter = greet.bind(steam);
+steamGreeter('Манго'); // "Манго, добро пожаловать в «Steam»."
+
+const gmail = { service: 'Gmail' };
+const gmailGreeter = greet.bind(gmail);
+gmailGreeter('Поли'); // "Поли, добро пожаловать в «Gmail»."
+Задание
+Функция composeMessage(customerName) создаёт приветственные сообщения для ресторанов. Дополни код так, чтобы в переменных pizzaPalaceComposer и burgerShackComposer были её копии с привязанным контекстом к соответствующим объектам. */
+
+const pizzaPalace = {
+  company: 'Pizza Palace',
+};
+
+const burgerShack = {
+  company: 'Burger Shack',
+};
+
+function composeMessage(customerName) {
+  return `${customerName}, всегда рады вас видеть в «${this.company}».`;
+}
+// Пиши код ниже этой строки
+
+const pizzaPalaceComposer = composeMessage.bind(pizzaPalace);
+const pizzaPalaceMessage = pizzaPalaceComposer('Манго');
+
+const burgerShackComposer = composeMessage.bind(burgerShack);
+const burgerShackMessage = burgerShackComposer('Поли');
+
+
+/*========================= Задача-10 =============================*/
 /*  */
-/*========================= Задача-1 =============================*/
-/*========================= Задача-1 =============================*/
-/*========================= Задача-1 =============================*/
--- test.js
+++ test.js
@@ -1,29 +1,19 @@
-const pizzaPalace = {
-    pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
-    order(pizzaName, onSuccess, onError) {
-     
-        if (this.pizzas.includes(pizzaName)) {
-          return onSuccess(pizzaName);
-      }
-   return onError(`В ассортименте нет пиццы с названием ${pizzaName}.`);
-    },
-  };
-  // Пиши код выше этой строки
-  
-  // Колбэк для onSuccess
-  function makePizza(pizzaName) {
-    return `Ваш заказ принят. Готовим пиццу ${pizzaName}.`;
-  }
-  
-  // Колбэк для onError
-  function onOrderError(error) {
-    return `Ошибка! ${error}`;
-  }
+const orders = [
+  { email: "solomon@topmail.ua", dish: "Burger" },
+  { email: "artemis@coldmail.net", dish: "Pizza" },
+  { email: "jacob@mail.com", dish: "Taco" },
+];
 
-// Вызовы метода с колбэками
-pizzaPalace.order('Аль Копчино', makePizza, onOrderError);
-pizzaPalace.order('Четыре нарезона', makePizza, onOrderError);
-pizzaPalace.order('Биг майк', makePizza, onOrderError);
-pizzaPalace.order('Венская', makePizza, onOrderError);
+// Пиши код ниже этой строки
+function composeMessage(position) {
+  return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
+}
+
+const messages = [];
+for (let i = 0; i < orders.length; i++) {
+  const msg = composeMessage.aplly(orders[i], [i + 1]);
+  messages.push(msg);
+}
+console.log(messages);
 
   
