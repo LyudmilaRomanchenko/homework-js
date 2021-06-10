0f46bc197e355681428a9fe9fa4e2db01704a497
LyudmilaRomanchenko <78047438+LyudmilaRomanchenko@users.noreply.github.com>
2021-06-10 22:37:55 +0300

    Добавляет 15 задач

--- index.js
+++ index.js
@@ -175,17 +175,514 @@ const calculateTotalPrice = (quantity, pricePerItem) => quantity * pricePerItem;
 // Пиши код выше этой строки
 
 
-/* ======================== Задача-1 ========================== */
-/* ======================== Задача-1 ========================== */
-/* ======================== Задача-1 ========================== */
-/* ======================== Задача-1 ========================== */
-/* ======================== Задача-1 ========================== */
-/* ======================== Задача-1 ========================== */
-/* ======================== Задача-1 ========================== */
-/* ======================== Задача-1 ========================== */
-/* ======================== Задача-1 ========================== */
-/* ======================== Задача-1 ========================== */
-/* ======================== Задача-1 ========================== */
+/* ======================== Задача-6 ========================== */
+/* Стрелочные функции как коллбеки
+Анонимные стрелочные функции отлично подходят как коллбеки для перебирающих методов массива из-за более краткого синтаксиса объявления, особенно если не нужно тело функции.
+
+const numbers = [5, 10, 15, 20, 25];
+
+// Объявление функции
+numbers.forEach(function (number, index) {
+  console.log(`Индекс ${index}, значение ${number}`);
+});
+
+// Анонимная стрелочная функция
+numbers.forEach((number, index) => {
+  console.log(`Индекс ${index}, значение ${number}`);
+});
+Стрелочную коллбек-функцию также можно объявлять отдельно и передавать на неё ссылку. Это стоит делать если одна функция используется в нескольих местах программы или если она громоздкая.
+
+const numbers = [5, 10, 15, 20, 25];
+
+const logMessage = (number, index) => {
+  console.log(`Индекс ${index}, значение ${number}`);
+};
+
+numbers.forEach(logMessage);
+Задание
+Выполни рефакторинг функции calculateTotalPrice(orderedItems) заменив её объявление на стрелочную функцию. Замени коллбек-функцию передаваемую в метод forEach() на стрелочную функцию. */
+
+// Пиши код ниже этой строки
+/*function calculateTotalPrice(orderedItems) {
+  let totalPrice = 0;
+
+  orderedItems.forEach(function (item) {
+    totalPrice += item;
+  });
+
+  return totalPrice;
+}*/
+const calculateTotalPrice = orderedItems => {
+  let totalPrice = 0;
+
+  orderedItems.forEach((item) => {
+    totalPrice += item;
+  });
+
+  return totalPrice;
+}
+// Пиши код выше этой строки
+
+/* ======================== Задача-7 ========================== */
+/* Задача. Фильтрация массива чисел 2.0
+Задание
+Замени объявление функции filterArray() и коллбек для метода forEach() на стрелочные функции. */
+/* // Пиши код ниже этой строки
+function filterArray(numbers, value) {
+    const filteredNumbers = [];
+  
+    numbers.forEach(function (number) {
+      if (number > value) {
+        filteredNumbers.push(number);
+      }
+    });
+  
+    // Пиши код выше этой строки
+    return filteredNumbers;
+  } */
+// Пиши код ниже этой строки
+const filterArray = (numbers, value) => {
+  const filteredNumbers = [];
+
+  numbers.forEach(number=> {
+    if (number > value) {
+      filteredNumbers.push(number);
+    }
+  });
+
+  // Пиши код выше этой строки
+  return filteredNumbers;
+}
+
+/* ======================== Задача-8 ========================== */
+/* Задача. Общие элементы 2.0
+Задание
+Замени объявление функции getCommonElements() и коллбек для метода 
+forEach() на стрелочные функции. */
+
+/* // Пиши код ниже этой строки
+function getCommonElements(firstArray, secondArray) {
+    const commonElements = [];
+  
+    firstArray.forEach(function (element) {
+      if (secondArray.includes(element)) {
+        commonElements.push(element);
+      }
+    });
+  
+    // Пиши код выше этой строки
+    return commonElements;
+  } */
+
+// Пиши код ниже этой строки
+const getCommonElements = (firstArray, secondArray) => {
+  const commonElements = [];
+
+  firstArray.forEach(element=> {
+    if (secondArray.includes(element)) {
+      commonElements.push(element);
+    }
+  });
+
+  // Пиши код выше этой строки
+  return commonElements;
+}
+
+
+/* ======================== Задача-9 ========================== */
+/* Чистые функции
+Функция с побочными эффектами - это функция которая в процессе выполнения может изменять или использовать глобальные переменные, изменять значение аргументов ссылочного типа, выполнять операции ввода-вывода и т. п.
+
+const dirtyMultiply = (array, value) => {
+  for (let i = 0; i < array.length; i += 1) {
+    array[i] = array[i] * value;
+  }
+};
+
+const numbers = [1, 2, 3, 4, 5];
+dirtyMultiply(numbers, 2);
+// Произошла мутация исходных данных - массива numbers
+console.log(numbers); // [2, 4, 6, 8, 10]
+Функция dirtyMultiply(array, value) умножает каждый элемент массива array на число value. Она изменяет (мутирует) исходный массив по ссылке.
+
+Чистая функция (pure function) - это функция результат которой зависит только от значений переданных аргументов. При одинаковых аргументах она всегда возвращает один и тот же результат и не имеет побочных эффектов, то есть не изменяет значения аргументов.
+
+Напишем реализацию чистой функции умножения элементов массива, возвращающей новый массив, не изменяя исходный.
+
+const pureMultiply = (array, value) => {
+  const newArray = [];
+
+  array.forEach(element => {
+    newArray.push(element * value);
+  });
+
+  return newArray;
+};
+
+const numbers = [1, 2, 3, 4, 5];
+const doubledNumbers = pureMultiply(numbers, 2);
+
+// Не произошло мутации исходных данных
+console.log(numbers); // [1, 2, 3, 4, 5]
+// Функция вернула новый массив с изменёнными данными
+console.log(doubledNumbers); // [2, 4, 6, 8, 10]
+Задание
+Функция changeEven(numbers, value) принимает массив чисел numbers и обновляет каждый элемент, значение которого это чётное число, добавляя к нему значение параметра value.
+
+Выполни рефакторинг функции так, чтобы она стала чистой - не изменяла массив чисел numbers, а создавала, наполняла и возвращала новый массив с обновлёнными значениями. */
+
+function changeEven(numbers, value) {
+  // Пиши код ниже этой строки
+  const newArray = [];
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] % 2 === 0) {
+      newArray.push(numbers[i] + value);
+    }
+    else {newArray.push(numbers[i]);}
+  }
+return newArray;
+  // Пиши код выше этой строки
+}
+
+/* ======================== Задача-10 ========================== */
+/* Метод map()
+Большинство перебирающих методов массива это чистые функции. Они создают новый массив, заполняют его, применяя к значению каждого элемента 
+указанную коллбек-функцию, после чего возвращают этот новый массив.
+
+Метод map(callback) используется для трансформации массива. Он вызывает коллбек-функцию для каждого элемента исходного массива, 
+а результат её работы записывает в новый массив, который и будет результатом выполнения метода.
+
+массив.map((element, index, array) => {
+  // Тело коллбек-функции
+});
+Поэлементно перебирает оригинальный массив.
+Не изменяет оригинальный массив.
+Результат работа коллбек-функции записывается в новый массив.
+Возвращает новый массив такой же длины.
+Его можно использовать для того, чтобы изменить каждый элемент массива. Оригинальный массив используется как эталон, 
+на базе которого можно сделать другую коллекцию.
+
+const planets = ['Земля', 'Марс', 'Венера', 'Юпитер'];
+
+const planetsInUpperCase = planets.map((planet) => planet.toUpperCase());
+console.log(planetsInUpperCase); // ['ЗЕМЛЯ', 'МАРС', 'ВЕНЕРА', 'ЮПИТЕР']
+
+const planetsInLowerCase = planets.map((planet) => planet.toLowerCase());
+console.log(planetsInLowerCase); // ['земля', 'марс', 'венера', 'юпитер']
+
+// Оригинальный массив не изменился
+console.log(planets); // ['Земля', 'Марс', 'Венера', 'Юпитер']
+Использование анонимных стрелочных функций с неявным возвратом сильно сокращает «шум» объявления коллбек-функции, делая код чище и проще для восприятия.
+
+Задание
+Дополни код так, чтобы в переменной planetsLengths получился массив длин названий планет. Обязательно используй метод map(). */
+
+const planets = ['Земля', 'Марс', 'Венера', 'Юпитер'];
+// Пиши код ниже этой строки
+const planetsLengths = planets.map(planet => planet.length);
+
+/* ======================== Задача-11 ========================== */
+/* Метод map() и массив объектов
+Мы уже знаем что повседневная задача это манипуляция массивом объектов. Например, получить массив значений свойства из всех объектов. Есть массив студентов, а нужно получить отдельный массив их имён.
+
+const students = [
+  { name: 'Манго', score: 83 },
+  { name: 'Поли', score: 59 },
+  { name: 'Аякс', score: 37 },
+  { name: 'Киви', score: 94 },
+  { name: 'Хьюстон', score: 64 }
+];
+
+const names = students.map(student => student.name);
+console.log(names); // ['Манго', 'Поли', 'Аякс', 'Киви', 'Хьюстон']
+Используя метод map() можно перебрать массив объектов, и в коллбек-функции вернуть значение свойства каждого из них.
+
+Задание
+Используя метод map() сделай так, чтобы в переменной titles получился массив названий книг (свойство title) из всех объектов массива books. */
+
+const books = [
+  { title: 'Последнее королевство', author: 'Бернард Корнуэлл', rating: 8.38 },
+  { title: 'На берегу спокойных вод', author: 'Роберт Шекли', rating: 8.51 },
+  { title: 'Сон смешного человека', author: 'Федор Достоевский', rating: 7.75 },
+  { title: 'Красна как кровь', author: 'Ли Танит', rating: 7.94 },
+  { title: 'Враг Божий', author: 'Бернард Корнуэлл', rating: 8.67 }
+];
+// Пиши код ниже этой строки
+
+const titles = books.map(book => book.title);
+
+/* ======================== Задача-12 ========================== */
+/* Метод flatMap()
+Метод flatMap(callback) аналогичен методу map(), но применяется в случаях, когда результат это многомерный массив который необходимо «разгладить».
+
+массив.flatMap((element, index, array) => {
+  // Тело коллбек-функции
+});
+В массиве students хранится список студентов со списком предметов, которые посещает студент, в свойстве courses. Несколько студентов могут посещать один и тот же предмет. Необходимо составить список всех предметов, которые посещает эта группа студентов, пока даже повторяющихся.
+
+const students = [
+  { name: 'Манго', courses: ['математика', 'физика'] },
+  { name: 'Поли', courses: ['информатика', 'математика'] },
+  { name: 'Киви', courses: ['физика', 'биология'] },
+];
+
+students.map((student) => student.courses);
+// [['математика', 'физика'], ['информатика', 'математика'], ['физика', 'биология']]
+
+students.flatMap((student) => student.courses);
+// ['математика', 'физика', 'информатика', 'математика', 'физика', 'биология'];
+Он вызывает коллбек-функцию для каждого элемента исходного массива, а результат её работы записывает в новый массив. Отличие от map() в том, что новый массив «разглаживается» на глубину равную единице (одна вложенность). Этот разглаженный массив и есть результат работы flatMap().
+
+Задание
+Используя метод flatMap() сделай так, чтобы в переменной genres получился массив всех жанров книг (свойство genres) из массива книг books. */
+
+const books = [
+  {
+    title: 'Последнее королевство',
+    author: 'Бернард Корнуэлл',
+    genres: ['приключения', 'историческое']
+  },
+  {
+    title: 'На берегу спокойных вод',
+    author: 'Роберт Шекли',
+    genres: ['фантастика']
+  },
+  {
+    title: 'Красна как кровь',
+    author: 'Ли Танит',
+    genres: ['ужасы', 'мистика']
+  }
+];
+// Пиши код ниже этой строки
+
+const genres = books.flatMap(book => book.genres);
+
+/* ======================== Задача-13 ========================== */
+/* Задача. Имена пользователей
+Этот массив объектов мы будем передавать в параметр users при вызове функции из задания.
+
+[
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    skills: ['ipsum', 'lorem'],
+    gender: 'male',
+    age: 37,
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
+    gender: 'female',
+    age: 34,
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+    gender: 'male',
+    age: 24,
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    skills: ['adipisicing', 'irure', 'velit'],
+    gender: 'female',
+    age: 21,
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    skills: ['ex', 'culpa', 'nostrud'],
+    gender: 'male',
+    age: 27,
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    skills: ['non', 'amet', 'ipsum'],
+    gender: 'male',
+    age: 38,
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    skills: ['lorem', 'veniam', 'culpa'],
+    gender: 'female',
+    age: 39,
+  },
+];
+Задание
+Дополни функцию getUserNames(users) так, чтобы она возвращала массив имён пользователей (свойство name) из массива объектов в параметре users. */
+
+// Пиши код ниже этой строки
+const getUserNames = users => {
+  return users.map(user => user.name);  
+ 
+   };
+   // Пиши код выше этой строки
+
+/* ======================== Задача-14 ========================== */
+/* Задача. Почты пользователей
+Этот массив объектов мы будем передавать в параметр users при вызове функции из задания.
+
+[
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    skills: ['ipsum', 'lorem'],
+    gender: 'male',
+    age: 37,
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
+    gender: 'female',
+    age: 34,
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+    gender: 'male',
+    age: 24,
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    skills: ['adipisicing', 'irure', 'velit'],
+    gender: 'female',
+    age: 21,
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    skills: ['ex', 'culpa', 'nostrud'],
+    gender: 'male',
+    age: 27,
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    skills: ['non', 'amet', 'ipsum'],
+    gender: 'male',
+    age: 38,
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    skills: ['lorem', 'veniam', 'culpa'],
+    gender: 'female',
+    age: 39,
+  },
+];
+Задание
+Дополни функцию getUserEmails(users) так, чтобы она возвращала массив почтовых адресов пользователей (свойство email) из массива объектов в параметре users. */
+
+// Пиши код ниже этой строки
+const getUserEmails = users => {
+  return users.map(user => user.email);  
+
+  };
+  // Пиши код выше этой строки
+
+/* ======================== Задача-15 ========================== */
+/* Методы filter и find
+Метод filter(callback) используется для единственной операции - фильтрации массива, то есть когда необходимо выбрать более одного элемента из коллекции по какому-то критерию.
+
+массив.filter((element, index, array) => {
+  // Тело коллбек-функции
+});
+Не изменяет оригинальный массив.
+Поэлементно перебирает оригинальный массив.
+Возвращает новый массив.
+Добавляет в возвращаемый массив элементы которые удовлетворяют условию коллбек-функции.
+Если коллбек вернул true элемент добавляется в возвращаемый массив.
+Если коллбек вернул false элемент не добавляется в возвращаемый массив.
+Если ни один элемент не удовлетворил условию, возвращает пустой массив.
+const values = [51, -3, 27, 21, -68, 42, -37];
+
+const positiveValues = values.filter(value => value >= 0);
+console.log(positiveValues); // [51, 27, 21, 42]
+
+const negativeValues = values.filter(value => value < 0);
+console.log(negativeValues); // [-3, -68, -37]
+
+const bigValues = values.filter(value => value > 1000);
+console.log(bigValues); // []
+
+// Оригинальный массив не изменился
+console.log(values); // [51, -3, 27, 21, -68, 42, -37]
+То есть метод filter вызывает коллбек-функцию для каждого элемента исходного массива и если результат её выполнения true, текущий элемент добавляет в новый массив.
+
+Задание
+Дополни код так, чтобы в переменной evenNumbers получился массив чётных чисел из массива numbers, а в переменной oddNumbers массив нечётных. Обязательно используй метод filter(). */
+
+const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// Пиши код ниже этой строки
+
+const evenNumbers = numbers.filter(number => number%2 === 0);
+const oddNumbers = numbers.filter(number => number%2 !== 0);
+
+/* ======================== Задача-16 ========================== */
+/*  */
 /* ======================== Задача-1 ========================== */
 /* ======================== Задача-1 ========================== */
 /* ======================== Задача-1 ========================== */
